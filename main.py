b05ff9fe3d82b22a7040e163d14dca8d9b0e3a3a
vavsar <sarcigar@gmail.com>
2021-07-27 20:07:13 +0400

    add yandex_weather function

--- main.py
+++ main.py
@@ -14,13 +14,7 @@ CONTENT_TYPE = "application/json"
 BASE_URL = "https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"
 
 
-@app.get("/", status_code=200)
-def read_root():
-    return 'OK'
-
-
-@app.get("/get_weather/", status_code=200)
-def get_weather(latitude: float, longitude: float):
+def yandex_weather(latitude, longitude):
     headers = {
         "X-Yandex-API-Key": APIKEY,
     }
@@ -29,7 +23,18 @@ def get_weather(latitude: float, longitude: float):
             BASE_URL.format(lat=latitude, lon=longitude),
             headers=headers
         ).json()
+        return response
     except ValueError as e:
         raise e
+
+
+@app.get("/", status_code=200)
+def read_root():
+    return 'OK'
+
+
+@app.get("/get_weather/", status_code=200)
+def get_weather(latitude: float, longitude: float):
+    response = yandex_weather(latitude, longitude)
     temperature = response.get('fact').get('temp')
     return {'temperature': temperature}
